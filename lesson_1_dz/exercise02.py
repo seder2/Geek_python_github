b432b22cfbe018e9df28cf4c35b207116f3e7352
Romans <romanspats@gmail.com>
2021-12-14 20:32:26 +0000

    updated exercise02 on lesson_1_dz

--- lesson_1_dz/exercise02.py
+++ lesson_1_dz/exercise02.py
@@ -1,3 +1,9 @@
 # 2. Пользователь вводит время в секундах.
 # Переведите время в часы, минуты и секунды и выведите
 # в формате чч:мм:сс. Используйте форматирование строк.
+
+introduced_seconds = int(input("Введите пожалуйста время в секундах :"))
+hours = introduced_seconds // 3600
+minutes = (introduced_seconds - hours * 3600) // 60
+seconds = (introduced_seconds - hours * 3600) % 60
+print(f"{hours}:{minutes}:{seconds}")

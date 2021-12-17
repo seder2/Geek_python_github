2f9ccb88fe1e2ea3eab96287f6406755732e6b24
Romans <romanspats@gmail.com>
2021-12-17 18:05:14 +0000

    updated lesson_1_dz/exercise03

--- lesson_1_dz/exercise03.py
+++ lesson_1_dz/exercise03.py
@@ -2,6 +2,11 @@
 # Например, пользователь ввёл число 3. Считаем 3 + 33 + 333 = 369.
 
 number_n = input("Введите целое число : ")
+
+if not number_n.isdigit():
+    print("Unknown format, sorry!")
+    exit()
+
 number_n_two = int(number_n + number_n)
 number_n_three = int(number_n + number_n + number_n)
 summa = int(number_n) + number_n_two + number_n_three

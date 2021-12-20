57d51a5e6720c0bc312248ff110927d91bbe795a
romanSpats <romanspats@gmail.com>
2021-12-20 19:20:08 +0000

    added hw_files to lesson_2_dz

--- lesson_2_dz/exercise_02_2.py
+++ lesson_2_dz/exercise_02_2.py
@@ -2,15 +2,9 @@
 # 1, 2 и 3 и т.д. При нечетном количестве элементов последний сохранить на своем месте. Для заполнения списка
 # элементов необходимо использовать функцию input().
 
-my_list = input("Введите элементы списка: ").split()
-print(my_list)
+my_list = input("Введите элементы списка через пробел: ").split()
+print("Заданый список :", my_list)
 
 my_list[:-1:2], my_list[1::2] = my_list[1::2], my_list[:-1:2]
-print(my_list)
+print("  Новый список :", my_list)
 
-
-# number_list = list("123456789")
-# print(number_list[1::2])
-# print(number_list[::2])
-# print(number_list[:-1:2])
-# print(number_list)
